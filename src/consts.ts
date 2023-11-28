cec0d3b6a33d0433d8062ee25095dccaf5fc01b3
Lorenzo Tomas Diez <lorenzotomasdiez@Lorenzos-Mac-mini.local>
2023-11-28 17:32:12 -0300

    remove some extra files

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dilo';
+export const SITE_DESCRIPTION = 'Discover the expertise of Dilo through a dynamic portfolio and insightful blog. Dive into impactful projects, professional insights, and in-depth articles. Navigate the journey of a dedicated professional, where knowledge meets innovation. Join us for a curated experience at the intersection of skill and exploration!';
